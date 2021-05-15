2be5a888eb2def87d0cda2ad6686918b6c93ae26
vr7517 <72854328+vr7517@users.noreply.github.com>
2021-05-15 14:44:56 +0530

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,22 +1,10 @@
-#!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
-import os
-import sys
+from flask import Flask
 
+from flask import render_template
+app= Flask (__name__)
 
-def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'powerbi.settings')
-    try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
-    execute_from_command_line(sys.argv)
+@app.route("/")
+def hello():
+    return render_template('index.html')
 
-
-if __name__ == '__main__':
-    main()
+app.run (debug=True)
